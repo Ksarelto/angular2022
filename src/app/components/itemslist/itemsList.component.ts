9be8c39fb74a9d5b655b6696720752fac71f7523
Artsiom_Murashko <Artsiom_Murashko@epam.com>
2022-10-02 21:27:09 +0300

    [angular_intro] Add necessary modules

---
+++ src/app/components/itemslist/itemsList.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-itemslist',
+  templateUrl: './itemslist.component.html',
+  styleUrls: ['./itemslist.component.scss'],
+})
+export class ItemsListComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
